3cd9b49c7159e4e1c2d0639c5a7fff1549fa9986
master312 <wowhellmail@gmail.com>
2015-08-13 20:00:17 +0200

    Test commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: master312
+ *
+ * Created on August 13, 2015, 7:58 PM
+ */
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+    
+    std::cout << "Test test, test!" << std::endl;
+    
+    return 0;
+}
+
