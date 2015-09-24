efdf6334bb2af19af3650e8a8a9d56f11c23b573
master312 <wowhellmail@gmail.com>
2015-09-24 19:23:20 +0200

    Updated storm version

--- src/main.cpp
+++ src/main.cpp
@@ -12,59 +12,21 @@
 #endif
 
 int main(int argc, char** argv) {
-//    lua_State* L = luaL_newstate();
-//    luaL_openlibs(L);
-//    getGlobalNamespace(L).addFunction("printMessage", printMessage);
-//    
-//    
-//    if (luaL_loadfile(L, "script.lua") || lua_pcall(L, 0, 0, 0)) {
-//        std::cout << "Script not loaded!" << std::endl;
-//        return -1;
-//    }
-//    std::cout << "Loaded lua"  << std::endl;
-//
-//    
-//    
-//    LuaRef s = getGlobal(L, "testString");
-//    LuaRef n = getGlobal(L, "number");
-//    
-//    if(s.isNil() || n.isNil()){
-//        std::cout << "Variable not found!|" << std::endl;
-//        return -1;
-//    }
-//    
-//    std::string luaString = s.cast<std::string>();
-//    int answer = n.cast<int>();
-//    std::cout << luaString << std::endl;
-//    std::cout << "And here's our number:" << answer << std::endl;
-//
-//    
-//    
-//    LuaRef t = getGlobal(L, "window");
-//    LuaRef title = t["title"];
-//    LuaRef w = t["width"];
-//    LuaRef h = t["height"];
-//    std::string titleString = title.cast<std::string>();
-//    int width = w.cast<int>();
-//    int height = h.cast<int>();
-//    std::cout << titleString << std::endl;
-//    std::cout << "width = " << width << std::endl;
-//    std::cout << "height = " << height << std::endl;
-//    
-//    
-//    
-//    LuaRef sumNumbers = t["sumNumbers"];
-//    //sumNumbers.isFunction()
-//    if(sumNumbers.isTable()){
-//        std::cout << "IsTable..." << std::endl;
-//    }
-//    
-//    if(!sumNumbers.isNil()) {
-//        int result = sumNumbers(5, 4, 4, 434, "milenium");
-//        std::cout << "ResultENd :" << result << std::endl;
-//    }else {
-//        std::cout << "SumNubers function not found in lua script" << std::endl;
-//    }
+        // doxygen comment example
+        //////////////////////////////////////////////////////////////////////////////////////////
+        /// static void GetRbgBonus(Player *pl, uint32 *honorWin, uint32 *honorLost, uint32 *arenaPoints)
+        /// \note   Calculates honor bonus for playing RBG
+        ///         If nullptr is passed for any argument except *arenaLost, it will cause crush
+        ///
+        /// \param  Player *pl  -  Pointer to player object
+        /// \param  uint32 *honorWin  -  Bonus honor for winning
+        /// \param  uint32 *honorLost -  Bonus honor for losing
+        /// \param  uint32 *arenaWin -  Arena points rewarded for winning
+        /// \param  uint32 *arenaLost -  Arena points rewarded losing
+        ///
+        /// \return none
+        ///
+        //////////////////////////////////////////////////////////////////////////////////////////
     
     cGame *g = new cGame();
     g->Start();
--- src/storm/defines.h
+++ src/storm/defines.h
@@ -23,10 +23,10 @@ typedef unsigned int uint;
 #include <GL/glu.h>
 #endif
 
-//Version 0.15.2
+//Version 0.15.8
 #define STORM_VERSION_MAJOR 0
 #define STORM_VERSION_MINOR 15
-#define STORM_VERSION_FIX 2
+#define STORM_VERSION_FIX 8
 //Rendering apis
 #define STORM_API_OPENGL 1
 #define STORM_API_SDL 2
